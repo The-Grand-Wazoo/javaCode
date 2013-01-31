1155e1b8206892d2e9b2f1b75cea424487085d1d
The-Grand-Wazoo <johnsinclair16@gmail.com>
2013-01-30 19:08:44 -0500

    First commit. Displays 8x8 grid.

---
+++ Checkers/src/Checkers/Main.java
@@ -0,0 +1,30 @@
+package Checkers;
+
+import java.awt.Container;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+
+public class Main
+{
+	public static void gameWindow()
+	{
+		JFrame window=new JFrame();
+		Container content=window.getContentPane();
+		Checkers game= new Checkers();
+		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(500,500);
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		content.setLayout(new GridLayout());
+		content.add(game);
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		gameWindow();
+
+	}
+
+}
