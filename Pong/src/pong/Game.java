e9c782e67c9bfb9226ba4a7555aac6e6308e4100
The-Grand-Wazoo <johnsinclair16@gmail.com>
2013-05-20 22:48:34 -0400

    initial commit

---
+++ Pong/src/pong/Game.java
@@ -0,0 +1,14 @@
+package pong;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Game extends JPanel
+{
+	public static void pieces(Graphics g)
+	{
+		
+	}
+	
+}
---
+++ Pong/src/pong/Pong.java
@@ -0,0 +1,11 @@
+package pong;
+
+public class Pong 
+{
+	public static void main(String[] args)
+	{
+		Window.createWindow();
+
+	}
+
+}
