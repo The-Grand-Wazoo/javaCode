f663284e92d8244ee60447cdc67edff9959f52ef
The-Grand-Wazoo <johnsinclair16@gmail.com>
2013-05-22 11:41:12 -0300

    Update Game.java

--- Pong/src/pong/Game.java
+++ Pong/src/pong/Game.java
@@ -1,12 +1,28 @@
 package pong;
 
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
 
 public class Game extends JPanel
 {
-	public static void pieces(Graphics g)
+	
+	static int y=0;
+	static int y1=50;
+	public void Game()
+	{
+		super.repaint();
+	}
+	public void paintComponent(Graphics g)
+	{
+		int width=getWidth();
+		int height=getHeight();
+		System.out.println("width: "+width);
+		System.out.println("height: "+height);
+		g.drawRect(0,y,20,100);
+		g.drawRect(height, y1, 20, 100);
+		
+	}
+	public static void move()
 	{
 		
 	}
