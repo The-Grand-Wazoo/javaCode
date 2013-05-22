de8ece5a96168add068105a3a675857958a75e10
The-Grand-Wazoo <johnsinclair16@gmail.com>
2013-05-22 11:41:51 -0300

    Update Pong.java

--- Pong/src/pong/Pong.java
+++ Pong/src/pong/Pong.java
@@ -1,11 +1,29 @@
 package pong;
 
+import java.awt.Container;
+import javax.swing.JFrame;
+
 public class Pong 
 {
+	static JFrame window= new JFrame();
+	static Container content=window.getContentPane();
+	
+	public static void createWindow()
+	{
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setSize(500,500);
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		window.setResizable(false);
+		
+	}
 	public static void main(String[] args)
 	{
-		Window.createWindow();
-
+		
+		createWindow();
+		Game game=new Game();
+		window.add(game);
 	}
 
 }
